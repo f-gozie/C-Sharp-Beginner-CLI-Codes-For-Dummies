a0accb6b2a566268ff8bd7ab21dd194d3ed417a7
Agozie <fagozie43@gmail.com>
2018-04-09 13:39:00 +0100

    Added some more stuff

---
+++ Others/CelsiusToFahrenheit/CelsiusToFahrenheit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CelsiusToFahrenheit
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double temperatureInCelsius, temperatureInFahrenheit;
+            
+            Console.Write("Enter temperature in Celsius: ");
+            temperatureInCelsius = Convert.ToInt32(Console.ReadLine());
+
+            temperatureInFahrenheit = temperatureInCelsius * (9 / 5 + 32);
+            Console.WriteLine(temperatureInFahrenheit);
+        }
+    }
+}
---
+++ Others/IzzyJacks/IzzyJacks/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace IzzyJacks
+{
+    internal class Program
+    {
+
+        static int evenSum()
+        {
+            int sum = 0;
+            int x = 0;
+            while (x <= 100)
+            {
+                sum = sum + x;
+                x += 2;
+            }
+
+            return sum;
+        }
+        
+        static int oddSum()
+        {
+            int sum = 0;
+            int x = 1;
+            while (x < 101)
+            {
+                sum = sum + x;
+                x += 2;
+            }
+
+            return sum;
+        }
+        
+        public static void Main(string[] args)
+        {
+            int totalSum, even, odd;
+            even = evenSum();
+            odd = oddSum();
+            totalSum = even + odd;
+            
+            Console.WriteLine(even);
+            Console.WriteLine(odd);
+            Console.WriteLine("Total sum of odd and even numbers between 1 and 100 is: {0}", totalSum);
+        }
+    }
+}
